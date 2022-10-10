4314402c1373c7783d2f0ea14703410d3a6bf54d
lidapengpeng <760715590@qq.com>
2022-10-10 16:27:03 +0800

    add code comment

--- 01-helloWord.py
+++ 01-helloWord.py
@@ -2,13 +2,15 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 if __name__ == '__main__':
-    # sys.argv相当于命令行输入的参数
+    # part1:创建一个事件检测对象，sys.argv相当于命令行输入的参数
     app = QApplication(sys.argv)
+    # part2:窗体展示的内容
+    # 创建界面对象
     w = QWidget()
     # 设置窗口标题
     w.setWindowTitle("第一个PyQT")
     # 显示窗口
     w.show()
-    # 程序进入循环等待
+    # part3:程序进入循环，等待检测
     app.exec_()
 
