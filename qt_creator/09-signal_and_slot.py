6a055e8076f7e29513ac0b23af33ea641f86fc84
lidapengpeng <760715590@qq.com>
2022-10-13 18:04:20 +0800

    some enhance model

--- qt_creator/09-signal_and_slot.py
+++ qt_creator/09-signal_and_slot.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import QApplication, QWidget
 class MyWindow(QWidget):
     def __init__(self):
         super(MyWindow, self).__init__()
+        self.text_browser = None
         self.passwd = None
         self.user_name = None
         self.ui = None
@@ -18,12 +19,19 @@ class MyWindow(QWidget):
         self.passwd = self.ui.lineEdit_2
         login_btn = self.ui.pushButton
         forget_btn = self.ui.pushButton_2
+        self.text_browser = self.ui.textEdit
         # 事件绑定
         login_btn.clicked.connect(self.login)
 
     def login(self):
-        print(self.user_name.text())
-        print(self.passwd.text())
+        user_name = self.user_name.text()
+        passwd = self.passwd.text()
+        if user_name == "admin" and passwd == "dp28ldp":
+            self.text_browser.setText("欢迎%s" % user_name)
+            self.text_browser.repaint()
+        else:
+            self.text_browser.setText("用户名或者密码错误...请重试")
+            self.text_browser.repaint()
 
 
 if __name__ == '__main__':
